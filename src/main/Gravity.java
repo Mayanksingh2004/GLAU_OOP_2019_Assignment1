6d780656f8982a4ab75c9e4fb3d3eaf74e3f1cc3
MayankSingh <mayank.singh1_cs18@gla.ac.in>
2019-08-23 17:11:16 +0530

    Imported Scanner Class

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -4,17 +4,13 @@
  * */
 package main;
 
-// import java.util.Scanner;
-// uncomment the above line to use input in your program.
+
+import java.util.Scanner;
 
 public class Gravity {
-	// TODO 1: Create the main method
+
 	public static void main(String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
+        Scanner scanner=new Scanner(System.in);
+
 	}
-	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
